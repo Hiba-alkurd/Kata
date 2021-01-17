0f134e87a4b3c2afa9577fcbd58a3fe097a3a166
Hiba Alkurd <hibaalkurd@gmail.com>
2021-01-17 13:21:15 +0200

    TAX

---
+++ Kata/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kata
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public int UPC { get; set; }
+        public double TaxRate { get; set; }
+        private double _price;
+
+        public Product(string name, int upc, double price )
+        {
+            _price = price;
+            Name = name;
+            UPC = upc;
+       
+        }
+
+        public double CalculatePriceWithTax(double tax = 0.2)
+        {
+            return Math.Round(_price*tax+ _price, 2);
+        }
+
+        public void DisplayPriceWithTax(double tax = 0.2)
+        {
+            Console.WriteLine($"Product price reported as ${_price} before tax and ${CalculatePriceWithTax(tax)} after {tax*100}% tax");
+        }
+
+
+    }
+}
